a8c9d831ca2b7bafd3c138b06bff96bda6a231c2
orangeberry <orangeberry@kakao.com>
2021-02-03 04:55:17 +0900

    uploads

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "25vh":"25vh",
+        "50vh":"50vh",
+        "75vh":"75vh",
+        'w-72': '18rem',
+        'w-84': '21rem',
+
+      }
+    },
   },
   variants: {
     extend: {},
