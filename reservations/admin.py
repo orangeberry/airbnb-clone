1bb83e5c50267bc6a286b6177cb24c9f73362821
orangeberry <orangeberry@kakao.com>
2021-02-09 06:15:12 +0900

    uploads

--- reservations/admin.py
+++ reservations/admin.py
@@ -18,3 +18,11 @@ class ReservationAdmin(admin.ModelAdmin):
     )
 
     list_filter = ("status",)
+
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+    list_display = (
+        "day",
+        "reservation",
+    )
